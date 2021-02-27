af51844dc20dbd0c92c542ab4db00e4ae20aede5
kbenjamin92 <kipp.benjamin@bryanuniversity.edu>
2021-02-27 15:47:48 -0500

    Exercise Complete

--- src/redux/reducer/reducer.js
+++ src/redux/reducer/reducer.js
@@ -5,7 +5,6 @@ export const initialState = {
 export const reducer = (state = initialState, action) => {
     switch (action.type) {
         case 'ADD_SEARCHED_TERM':
-            console.log(state.searchCollection)
             return {
                 searchCollection: [action.payload, ...state.searchCollection]
             }
